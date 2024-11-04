83392c5890055aac520c63f59a02940acda2ad47
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-11-04 12:45:53 +0100

    Ejercicio 31 finalizado

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,26 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
 
+        String filePath = "src/main/resources/lorem.txt";
+        leerYImprimirFichero(filePath);
     }
+
+    public static void leerYImprimirFichero(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el fichero: " + e.getMessage());
+        }
+    }
+
 }
